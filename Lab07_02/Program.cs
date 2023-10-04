1ff2b06330f15f92c64ef8b5d447bdc5a99df5e1
Sandra Carolina <Sandra Carolina@DESKTOP-SFK7P55>
2023-10-03 21:38:23 -0500

    Agregar archivos de proyecto.

---
+++ Lab07_02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Números divisibles entre 3 del 1 al 1000:");
+
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine($"Divisor de Omar Orlandini {i / 3} : {i}");
+                }
+            }
+            
+            
+        }
+    }
+}
